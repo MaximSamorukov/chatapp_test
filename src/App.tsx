7422ffcf1bb896d666f0335a1f926b955696e086
maksim.samorukov <maxim.samorukov@gmail.com>
2026-01-28 00:04:18 +0300

    feat: filter done

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,27 @@ import { ControlPanel } from './components/ControlPanel';
 import { InputField } from './components/InputField';
 import { TodoItem } from './components/TodoItem';
 import { useAppSelector } from './state';
+import { useState } from 'react';
+import type { FilterType } from './types';
+
+
 
 function App() {
-  const { todos } = useAppSelector((s) => s.todos)
+  const [filter, setFilter] = useState<FilterType>('all');
+  
+  const todos = useAppSelector((s) => {
+    if (filter === 'done') {
+      return s.todos.todos.filter((i) => i.isDone)
+    }
+    if (filter === 'not_done') {
+      return s.todos.todos.filter((i) => !i.isDone)
+    }
+    return s.todos.todos
+  })
   return (
     <div className={s.container}>
       <InputField />
-      <ControlPanel />
+      <ControlPanel filter={filter} setFilter={setFilter}/>
       <div className={cn(s.todoItemsContainer, {[s.empty]: !todos.length})}>
         {todos.map((i) => (
           <TodoItem key={i.id} id={i.id} title={i.title} isDone={i.isDone} />
--- src/components/ControlPanel/index.tsx
+++ src/components/ControlPanel/index.tsx
@@ -1,13 +1,43 @@
-import React from 'react'
-import s from './style.module.scss';
-type ControlPanelProps = {
+import React from 'react';
+import cn from 'classnames';
+import DoneIcon  from '../../assets/done.svg?react';
+import NotDoneIcon  from '../../assets/not_done.svg?react';
 
+import s from './style.module.scss';
+import type { FilterType } from '../../types';
+
+type ControlPanelProps = {
+  filter: FilterType
+  setFilter: (arg: FilterType) => void
 }
 
-export const ControlPanel: React.FC<ControlPanelProps> = () => {
-  
+
+
+export const ControlPanel: React.FC<ControlPanelProps> = ({ filter, setFilter }) => {
+  const toggleDoneFilter = () => {
+    if (filter === 'done') {
+      setFilter('all');
+    } else {
+      setFilter('done')
+    }
+  }
+  const toggleNotDoneFilter = () => {
+    if (filter === 'not_done') {
+      setFilter('all');
+    } else {
+      setFilter('not_done')
+    }
+  }
   return (
-    <div className={s.container}>control panel</div>
+    <div className={s.container}>
+      <button onClick={toggleDoneFilter} className={cn(s.filter, {[s.active]: filter === 'done'})}>
+        <DoneIcon />
+      </button>
+      <button onClick={toggleNotDoneFilter} className={cn(s.filter, {[s.active]: filter === 'not_done'})}>
+        <NotDoneIcon />
+      </button>
+
+    </div>
   )
 }
 
