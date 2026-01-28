0885f9527c643cb64baa710c110c805834907ce9
maksim.samorukov <maxim.samorukov@gmail.com>
2026-01-28 14:28:50 +0300

    added memo

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, type ChangeEvent } from "react";
+import React, { memo, useState, type ChangeEvent } from "react";
 import { useSortable } from "@dnd-kit/sortable";
 import CloseIcon from "../../assets/close.svg?react";
 import DoneIcon from "../../assets/done.svg?react";
@@ -21,76 +21,83 @@ type TodoItemProps = {
   id: TodoItemType["id"];
 };
 
-export const TodoItem: React.FC<TodoItemProps> = ({ id, title, isDone }) => {
-  const { attributes, listeners, setNodeRef, transform } = useSortable({
-    id,
-    transition: {
-      duration: 150,
-      easing: "cubic-bezier(0.25, 1, 0.5, 1)",
-    },
-  });
-  const [editState, setEditState] = useState(false);
-  const [editedTitle, setEditedTitle] = useState("");
-  const style = transform
-    ? {
-        transform: `translate(0px, ${transform.y}px)`,
-      }
-    : undefined;
-  const dispatch = useAppDispatch();
+export const TodoItem: React.FC<TodoItemProps> = memo(
+  ({ id, title, isDone }) => {
+    const { attributes, listeners, setNodeRef, transform } = useSortable({
+      id,
+      transition: {
+        duration: 150,
+        easing: "cubic-bezier(0.25, 1, 0.5, 1)",
+      },
+    });
+    const [editState, setEditState] = useState(false);
+    const [editedTitle, setEditedTitle] = useState("");
+    const style = transform
+      ? {
+          transform: `translate(0px, ${transform.y}px)`,
+        }
+      : undefined;
+    const dispatch = useAppDispatch();
 
-  const removeItem = () => {
-    dispatch(removeTodo({ id }));
-  };
-  const toggleTodo = () => {
-    dispatch(toggleTodoState({ id }));
-  };
-  const handleEdit = () => {
-    if (!editState) {
-      setEditedTitle(title);
-      setEditState(true);
-    } else {
+    const removeItem = () => {
+      dispatch(removeTodo({ id }));
+    };
+    const toggleTodo = () => {
+      dispatch(toggleTodoState({ id }));
+    };
+    const handleEdit = () => {
+      if (!editState) {
+        setEditedTitle(title);
+        setEditState(true);
+      } else {
+        if (editedTitle !== title) {
+          dispatch(updateTodo({ id, isDone, title: editedTitle }));
+        }
+        setEditState(false);
+      }
+    };
+    const onTitleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+      const text = e.currentTarget.value;
+      setEditedTitle(text);
+    };
+    const handleBlur = () => {
       if (editedTitle !== title) {
         dispatch(updateTodo({ id, isDone, title: editedTitle }));
       }
       setEditState(false);
-    }
-  };
-  const onTitleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const text = e.currentTarget.value;
-    setEditedTitle(text);
-  };
-  const handleBlur = () => {
-    if (editedTitle !== title) {
-      dispatch(updateTodo({ id, isDone, title: editedTitle }));
-    }
-    setEditState(false);
-  };
-  return (
-    <div ref={setNodeRef} style={style} className={s.container} {...attributes}>
-      {editState ? (
-        <input
-          onBlur={handleBlur}
-          className={s.inputTitle}
-          value={editedTitle}
-          onChange={onTitleInputChange}
-        />
-      ) : (
-        <div className={s.title}>{title}</div>
-      )}
-      <div className={s.controls}>
-        <button {...listeners} className={s.control}>
-          <DragIcon />
-        </button>
-        <button onClick={toggleTodo} className={s.control}>
-          {isDone ? <DoneIcon /> : <NotDoneIcon />}
-        </button>
-        <button onClick={handleEdit} className={s.control}>
-          {editState ? <EditInProgressIcon /> : <EditIcon />}
-        </button>
-        <button onClick={removeItem} className={s.control}>
-          <CloseIcon />
-        </button>
+    };
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className={s.container}
+        {...attributes}
+      >
+        {editState ? (
+          <input
+            onBlur={handleBlur}
+            className={s.inputTitle}
+            value={editedTitle}
+            onChange={onTitleInputChange}
+          />
+        ) : (
+          <div className={s.title}>{title}</div>
+        )}
+        <div className={s.controls}>
+          <button {...listeners} className={s.control}>
+            <DragIcon />
+          </button>
+          <button onClick={toggleTodo} className={s.control}>
+            {isDone ? <DoneIcon /> : <NotDoneIcon />}
+          </button>
+          <button onClick={handleEdit} className={s.control}>
+            {editState ? <EditInProgressIcon /> : <EditIcon />}
+          </button>
+          <button onClick={removeItem} className={s.control}>
+            <CloseIcon />
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  },
+);
