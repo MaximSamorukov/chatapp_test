5f3d309b446446a4016ff047f8adf55d0b13bd84
maksim.samorukov <maxim.samorukov@gmail.com>
2026-01-28 13:05:47 +0300

    feat: sortable functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
 
 import  s from './app.module.scss';
 import cn from 'classnames';
+import {
+  DndContext, 
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  type DragEndEvent,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
 import { ControlPanel } from './components/ControlPanel';
 import { InputField } from './components/InputField';
 import { TodoItem } from './components/TodoItem';
-import { selector, useAppSelector } from './state';
-import { useMemo, useState } from 'react';
+import { selector, useAppSelector, type TodoItem as TodoItemType } from './state';
+import { useEffect, useMemo, useState } from 'react';
 import type { FilterType } from './types';
 import { FILTER } from './constants';
 
-
-
 function App() {
   const [filter, setFilter] = useState<FilterType>(FILTER.ALL);
+  const [todos, setTodos] = useState<TodoItemType[]>([])
   const originalTodos = useAppSelector(selector)
 
-  const todos = useMemo(() => {
-        if (filter === FILTER.DONE) {
+  const filteredTodos = useMemo(() => {
+    if (filter === FILTER.DONE) {
       return originalTodos.filter((i) => i.isDone)
     }
     if (filter === FILTER.NOT_DONE) {
@@ -25,15 +39,49 @@ function App() {
     return originalTodos
   }, [filter, originalTodos])
 
+  useEffect(() => {
+    setTodos(() => filteredTodos)
+  }, [filteredTodos])
+  
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+  const handleDragEnd = (event: DragEndEvent) => {
+    const {active, over} = event;
+    
+    if (active.id !== over?.id) {
+      setTodos((items) => {
+        const oldIndex = items.findIndex((item) => item.id === active.id);
+        const newIndex = items.findIndex((item) => item.id === over?.id);
+        
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
+
   const emptyText = originalTodos.length === 0 ? 'У вас нет todos' : `У вас нет ${filter === FILTER.DONE ? 'выполненных' : 'невыполненных'} todos`
   return (
     <div className={s.container}>
       <InputField />
       <ControlPanel filter={filter} setFilter={setFilter}/>
       <div className={cn(s.todoItemsContainer, {[s.empty]: !todos.length})}>
-        {todos.map((i) => (
-          <TodoItem key={i.id} id={i.id} title={i.title} isDone={i.isDone} />
-        ))}
+        <DndContext
+          sensors={sensors}
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+        >
+          <SortableContext 
+            items={todos}
+            strategy={verticalListSortingStrategy}
+          >
+            {todos.map((i) => (
+              <TodoItem key={i.id} id={i.id} title={i.title} isDone={i.isDone} />
+            ))}
+          </SortableContext>
+        </DndContext>
         {!todos.length && <div>{emptyText}</div>}
       </div>
     </div>
--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,11 +1,11 @@
-import React, { useState, type ChangeEvent } from 'react'
+import React, { useState, type ChangeEvent } from 'react';
+import {useSortable} from '@dnd-kit/sortable';
 import CloseIcon from '../../assets/close.svg?react'
 import DoneIcon from '../../assets/done.svg?react';
 import NotDoneIcon from '../../assets/not_done.svg?react';
 import EditIcon from '../../assets/edit.svg?react';
 import EditInProgressIcon from '../../assets/edit_inprogress.svg?react';
 import DragIcon from '../../assets/drag.svg?react';
-import cn from 'classnames';
 import s from './style.module.scss';
 import { removeTodo, toggleTodoState, updateTodo, useAppDispatch, type TodoItem as TodoItemType } from '../../state';
 
@@ -16,9 +16,26 @@ type TodoItemProps = {
 }
 
 export const TodoItem: React.FC<TodoItemProps> = ({ id, title, isDone }) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({
+        id,
+        transition: {
+          duration: 150,
+          easing: 'cubic-bezier(0.25, 1, 0.5, 1)',
+        },
+      });
   const [editState, setEditState] = useState(false);
   const [editedTitle, setEditedTitle] = useState('');
-  const dispatch = useAppDispatch()
+  const style = transform ? {
+    transform: `translate(0px, ${transform.y}px)`,
+  } : undefined;
+  const dispatch = useAppDispatch();
+
   const removeItem = () => {
     dispatch(removeTodo({ id }))
   }
@@ -46,7 +63,7 @@ export const TodoItem: React.FC<TodoItemProps> = ({ id, title, isDone }) => {
     }
   }
   return (
-    <div className={s.container}>
+    <div ref={setNodeRef} style={style} className={s.container} {...attributes}>
         {editState ? (
           <input onBlur={handleBlur} className={s.inputTitle} value={editedTitle} onChange={onTitleInputChange} />
         ) : (
@@ -54,20 +71,20 @@ export const TodoItem: React.FC<TodoItemProps> = ({ id, title, isDone }) => {
             {title}
           </div>
         )}
-      <div className={s.controls}>
-        <button className={s.control}>
-          <DragIcon />
-        </button>
-        <button onClick={toggleTodo} className={s.control}>
-          {isDone ? (<DoneIcon />) : (<NotDoneIcon/>)}
-        </button>
-        <button onClick={handleEdit} className={s.control}>
-          {editState ? (<EditInProgressIcon />) : (<EditIcon />)}
-        </button>
-        <button onClick={removeItem} className={s.control}>
-          <CloseIcon />
-        </button>
-      </div>
+        <div className={s.controls}>
+          <button {...listeners} className={s.control}>
+            <DragIcon />
+          </button>
+          <button onClick={toggleTodo} className={s.control}>
+            {isDone ? (<DoneIcon />) : (<NotDoneIcon/>)}
+          </button>
+          <button onClick={handleEdit} className={s.control}>
+            {editState ? (<EditInProgressIcon />) : (<EditIcon />)}
+          </button>
+          <button onClick={removeItem} className={s.control}>
+            <CloseIcon />
+          </button>
+        </div>
       </div>
   )
 }
